c5641f45699902468496dc8e25b9fef031ebca48
Diego Perez <diegopr178@gmail.com>
2021-02-28 19:07:17 -0300

    FIX: redundant self friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  after_save :friend_self
+  after_create :friend_self
 
   has_many :posts, dependent: :destroy
   has_many :friends, dependent: :destroy
