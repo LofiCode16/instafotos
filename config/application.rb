e8c58881dd3bdf3b6517eb47b44f856e9ed62d4b
Diego perez <diegopr178@gmail.com>
2021-06-08 17:05:53 -0400

    UPDATE: cloud storage

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Instafotos
 
     # Set locale
     config.i18n.default_locale = :es
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-images = [
-    'https://www.hola.com/imagenes/estar-bien/20210217184541/gatos-gestos-lenguaje-significado/0-922-380/gatos-gestos-m.jpg',
-    'https://www.ecestaticos.com/image/clipping/79776773aab795837282c7d4947abaf7/por-que-nos-parece-que-los-perros-sonrien-una-historia-de-30-000-anos.jpg',
-    'https://t1.ea.ltmcdn.com/es/images/9/1/3/tipos_de_erizos_de_tierra_20319_orig.jpg'
-]
+# images = [
+#     'https://www.hola.com/imagenes/estar-bien/20210217184541/gatos-gestos-lenguaje-significado/0-922-380/gatos-gestos-m.jpg',
+#     'https://www.ecestaticos.com/image/clipping/79776773aab795837282c7d4947abaf7/por-que-nos-parece-que-los-perros-sonrien-una-historia-de-30-000-anos.jpg',
+#     'https://t1.ea.ltmcdn.com/es/images/9/1/3/tipos_de_erizos_de_tierra_20319_orig.jpg'
+# ]
 
-3.times do |i|
-    User.create(
-        name: "user#{i+1}",
-        email: "user#{i+1}@mail.com",
-        password: '123456'
-    )
-end
+# 3.times do |i|
+#     User.create(
+#         name: "user#{i+1}",
+#         email: "user#{i+1}@mail.com",
+#         password: '123456'
+#     )
+# end
 
-20.times do |i|
-    Post.create(
-        image_url: images.sample,
-        content: "contenido del post #{i+1}",
-        user_id: User.all.sample.id
-    )
-end
+# 20.times do |i|
+#     Post.create(
+#         image_url: images.sample,
+#         content: "contenido del post #{i+1}",
+#         user_id: User.all.sample.id
+#     )
+# end
 
